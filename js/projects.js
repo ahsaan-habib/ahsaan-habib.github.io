14700bbe101e2b9686410addf2e2ce59014ff63d
Ahsan Habib <54646596+ahsaan-habib@users.noreply.github.com>
2022-06-25 00:58:09 +0600

    projects added & nav with js added

--- js/projects.js
+++ js/projects.js
@@ -1,88 +1,120 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const togglenav = document.querySelector("#togglenav");
+  const menuIcon = document.querySelector("#menu_icon");
+  const slicknavbar = document.querySelector("#navbarSupportedContent");
 
+  const sLinks = document.querySelectorAll(".s-menu a");
+  const socialIcons = document.querySelectorAll(".social-icons li");
 
-    const togglenav = document.querySelector('#togglenav');
-    const menuIcon = document.querySelector('#menu_icon');
-    const slicknavbar = document.querySelector('#navbarSupportedContent');
+  var toptiptapCount = 0;
 
-    const sLinks = document.querySelectorAll('.s-menu a');
-    const socialIcons = document.querySelectorAll('.social-icons li');
+  socialIcons.forEach((icon) => {
+    const tooltip = icon.children[1];
 
-    var toptiptapCount = 0;
-    
-    socialIcons.forEach(icon => {
-        const tooltip = icon.children[1];
-        
-        icon.onclick = (e) => {
-            if(tooltip != undefined && toptiptapCount > 0){
-                tooltip.style.display == "none" ? tooltip.style.display = "block" : tooltip.style.display = "none";
-                
-            }
-            toptiptapCount ++;
-        }
-        
-    });
+    icon.onclick = (e) => {
+      if (tooltip != undefined && toptiptapCount > 0) {
+        tooltip.style.display == "none"
+          ? (tooltip.style.display = "block")
+          : (tooltip.style.display = "none");
+      }
+      toptiptapCount++;
+    };
+  });
 
-    sLinks.forEach(link => {
-        link.onclick = () => {
-            slicknavbar.classList.remove('show');
-            menuIcon.className = "menu_icon_wrapper";
-        }
-    });
-    
-    togglenav.addEventListener("click", () =>{
-        if(menuIcon.className == "menu_icon_wrapper"){
-            menuIcon.className = "active_menu_icon_wrapper";
-        }
-        else{
-            menuIcon.className = "menu_icon_wrapper";
-        }
-    })
+  sLinks.forEach((link) => {
+    link.onclick = () => {
+      slicknavbar.classList.remove("show");
+      menuIcon.className = "menu_icon_wrapper";
+    };
+  });
 
+  togglenav.addEventListener("click", () => {
+    if (menuIcon.className == "menu_icon_wrapper") {
+      menuIcon.className = "active_menu_icon_wrapper";
+    } else {
+      menuIcon.className = "menu_icon_wrapper";
+    }
+  });
 
+  window.addEventListener("scroll", () => {
+    stickyNavbar("customNavbar");
+  });
 
-    window.addEventListener('scroll', ()=>{
+  function stickyNavbar(id) {
+    const socialIconsBar = document.querySelector("#social-icons-bar");
+    const headerContent = document.querySelector("#header-content");
 
-        stickyNavbar('customNavbar');
+    const cnavbar = document.querySelector(`#${[id]}`);
+    const sticky = cnavbar.offsetTop;
+    if (window.pageYOffset >= sticky + 300) {
+      cnavbar.classList.add("NavbarSticky");
+      socialIconsBar.classList.add("fixed-s-i");
+      socialIconsBar.previousElementSibling.style.display = "block";
+      headerContent.classList.add("sticky-content");
+    } else {
+      cnavbar.classList.remove("NavbarSticky");
+      socialIconsBar.classList.remove("fixed-s-i");
+      socialIconsBar.previousElementSibling.style.display = "none";
+      headerContent.classList.remove("sticky-content");
+    }
+  }
 
-    })
+  toggleNavLinkClass("menu");
+  toggleNavLinkClass("s-menu");
 
-
-    function stickyNavbar(id){
-        const socialIconsBar = document.querySelector('#social-icons-bar');
-        
-		const cnavbar = document.querySelector(`#${[id]}`);
-		const sticky = cnavbar.offsetTop;
-		if(window.pageYOffset >= sticky+300){
-			cnavbar.classList.add('NavbarSticky');
-            socialIconsBar.classList.add('fixed-s-i');
-            socialIconsBar.previousElementSibling.style.display = "block";
-		}
-		else{
-			cnavbar.classList.remove('NavbarSticky');
-            socialIconsBar.classList.remove('fixed-s-i');
-            socialIconsBar.previousElementSibling.style.display = "none";
-		}
-	}
-
-    toggleNavLinkClass('menu');
-    toggleNavLinkClass('s-menu');
-
-    // cann't sync between different viewport 
-    function toggleNavLinkClass(menu){
-        const navItems = document.querySelectorAll(`.${menu} li`);
-        navItems.forEach(navItem => {
-            navItem.onclick = () => {
-                navItems.forEach(item => {
-                    if(item.classList.contains('active')){
-                        item.classList.remove('active');
-                    }
-                })
-                navItem.classList.add('active');
-
-            }
-
+  // cann't sync between different viewport
+  function toggleNavLinkClass(menu) {
+    const navItems = document.querySelectorAll(`.${menu} li`);
+    navItems.forEach((navItem) => {
+      navItem.onclick = () => {
+        navItems.forEach((item) => {
+          if (item.classList.contains("active")) {
+            item.classList.remove("active");
+          }
         });
-    }  
+        navItem.classList.add("active");
+      };
+    });
+  }
 
+  const projectNavLink = document.querySelectorAll(".dropdown-link");
+  const projects = document.querySelectorAll(".project-class");
+
+  const url = window.location;
+  if (url.href.includes("#")) {
+    console.log(url.hash);
+    projects.forEach((project) => {
+      project.style.display = "none";
+    });
+    const projectType = document.querySelectorAll(
+      `.${url.hash.slice(1)}-project`
+    );
+
+    projectType.forEach((project) => {
+      project.style.display = "block";
+    });
+    if (projectType.length === 0) {
+      projects.forEach((project) => {
+        project.style.display = "block";
+      });
+    }
+  } else {
+    projects.forEach((project) => {
+      project.style.display = "block";
+    });
+  }
+
+  projectNavLink.forEach((projectLink) => {
+    projectLink.onclick = () => {
+      projects.forEach((project) => {
+        project.style.display = "none";
+      });
+      const projectType = document.querySelectorAll(
+        `.${projectLink.hash.slice(1)}-project`
+      );
+      projectType.forEach((project) => {
+        project.style.display = "block";
+      });
+    };
+  });
 });
